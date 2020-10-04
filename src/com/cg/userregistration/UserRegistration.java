375e2ca055a750d537095ed4023b8a264a6aebe6
Ard2238 <abhishekrdas28@gmail.com>
2020-10-04 14:09:59 +0530

    Refactored Code to validate first name

--- src/com/cg/userregistration/UserRegistration.java
+++ src/com/cg/userregistration/UserRegistration.java
@@ -1,12 +1,21 @@
 package com.cg.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-	
+	/* UC1 -- Validate First Name */
+	private static boolean validateName(String firstName) {
+		return Pattern.matches("[A-Z][a-z]{2,}", firstName);
+	}
 	
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);		
 		System.out.println("Welcom to User Registration Problem");
-		
+		System.out.println("Enter the first name: ");
+		String firstName = sc.next();
+		System.out.println(validateName(firstName)? "Valid First Name." : "Invalid First Name.");
+		sc.close();
 	}
 
 }
