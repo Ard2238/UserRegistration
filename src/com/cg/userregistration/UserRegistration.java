84a9fdbbf5cdb8ec6c6aa2329e7cd4b2c1baebd3
Ard2238 <abhishekrdas28@gmail.com>
2020-10-04 14:06:21 +0530

    Welcome to the User Registration Program

---
+++ src/com/cg/userregistration/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.cg.userregistration;
+
+public class UserRegistration {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcom to User Registration Problem");
+		
+	}
+
+}
