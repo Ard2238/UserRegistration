5523afa96ba5b681416ecf833654dc7a1f3980b9
Ard2238 <abhishekrdas28@gmail.com>
2020-10-05 09:14:17 +0530

    Refactored Code to add registration functionalities

--- src/com/cg/userregistration/UserRegistration.java
+++ src/com/cg/userregistration/UserRegistration.java
@@ -6,19 +6,46 @@ import java.util.regex.Pattern;
 public class UserRegistration {	
 	private static Scanner sc = new Scanner(System.in);
 	
+	private String firstName, lastName,email,number,password;
+	public UserRegistration(String firstName, String lastName, String email, String number, String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.number = number;
+		this.password = password;
+	}
+	
+	private void initializeDetails() {
+		boolean checkFName = validateName(firstName);
+		boolean checkLName = validateName(lastName);
+		boolean checkEMail = validateEmail(email);
+		boolean checkMobile = validateMobile(number);
+		boolean checkPass = validatePassword(password);
+		System.out.println(checkFName? "Valid First Name." : "Invalid First Name.");
+		System.out.println(checkLName? "Valid Last Name." : "Invalid Last Name.");
+		System.out.println(checkEMail? "Valid Email." : "Invalid Email.");
+		System.out.println(checkMobile? "Valid Mobile Number" : "Invalid Mobile Number");
+		System.out.println(checkPass? "Valid Password." : "Invalid Password.");
+		if( checkFName && checkLName && checkEMail && checkMobile && checkPass)
+			System.out.println("User Registered Successfully.");
+		else
+			System.out.println("One or more fields are incorrect. Registration Unsuccessful.");
+	}
+	
 	/* UC1 -- Validate First Name */
 	/* UC2 -- Validate Last Name */
-	private static boolean validateName(String firstName) {
+	private boolean validateName(String firstName) {
 		return Pattern.matches("[A-Z][a-z]{2,}", firstName);
 	}
 	
 	/* UC3 -- Validate Email */
-	private static boolean validateEmail(String email) {		
+	private boolean validateEmail(String email) {		
 		return Pattern.matches("[a-z]+(.[a-z0-9]+)*@{1}[a-z0-9]+[.][a-z]{2,}(.[a-z]+)*", email);
 	}
 	
 	/*UC4 -- Validate Mobile Number */
-	private static boolean validateMobile(String number) {
+	private boolean validateMobile(String number) {
 		return Pattern.matches("[0-9]{2}\\s[0-9]{10}", number);
 	}
 	
@@ -28,31 +55,25 @@ public class UserRegistration {
 	 * Rule 3. At least one numeric character
 	 * Rule 4. Exactly one special character
 	 * */
-	private static boolean validatePassword(String password) {
+	private boolean validatePassword(String password) {
 		return Pattern.matches("(?=.*\\d)(?=.*[A-Z])?(?=.*\\W).{8,}", password);
 	}
 	
 	public static void main(String[] args) {		
 		System.out.println("Welcome to User Registration Problem");
 		System.out.println("Enter the first name: ");
-		String firstName = sc.next();
-		System.out.println(validateName(firstName)? "Valid First Name." : "Invalid First Name.");
-		
+		String firstName = sc.next();		
 		System.out.println("Enter the last name: ");
-		String lastName = sc.next();
-		System.out.println(validateName(lastName)? "Valid Last Name." : "Invalid Last Name.");
-		
+		String lastName = sc.next();		
 		System.out.println("Enter the email: ");
-		String email = sc.next();
-		System.out.println(validateEmail(email)? "Valid Email." : "Invalid Email.");
-		
+		String email = sc.next(); sc.nextLine();
 		System.out.println("Enter the Mobile Number: ");
 		String number = sc.nextLine();
-		System.out.println(validateMobile(number)? "Valid Mobile Number" : "Invalid Mobile Number");
-		
 		System.out.println("Enter the Password: ");
 		String password = sc.next();
-		System.out.println(validatePassword(password)? "Valid Password." : "Invalid Password.");
+		
+		UserRegistration newUser = new UserRegistration(firstName, lastName, email, number, password);
+		newUser.initializeDetails();
 		sc.close();
 	}
 
